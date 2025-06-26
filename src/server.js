6620d6e10f1854ec0983596781e8625b72cfa1d5
Gowtham Reddy <gowthamreddy3010@gmail.com>
2025-06-26 13:52:17 +0530

    SET UP DONE

--- src/server.js
+++ src/server.js
@@ -1,1 +1,10 @@
-import dotenv from "dotenv";
+import dotenvb from "dotenv";
+import { ConnectDB } from "./config/dbConnect";
+import app from "./app"
+dotenv.config();
+ConnectDB();
+const PORT = 8080;
+app.listen(PORT,()=>{
+    console.log(`server is running at http://localhost:${PORT}`);
+})
+
