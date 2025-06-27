be1dfcb1993210e4138a3f8e036b40dc927fbadf
Gowtham Reddy <gowthamreddy3010@gmail.com>
2025-06-27 13:50:55 +0530

    solved a small error

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -7,7 +7,7 @@ class ApiResponse{
         this.statusCode = statusCode
         this.data = data
         this.message = message
-        this.success = statuscode<400
+        this.success = statusCode<400
     }
 }
 export{ApiResponse}
